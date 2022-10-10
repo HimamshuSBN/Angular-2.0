7c4b595f4e4140b00c8f8f2669ad880d8e127a92
Himamshu Bengaluru <hbn@Himamshus-MBP.lan>
2022-10-10 12:08:18 -0500

    MAXAN compnents

---
+++ Angular/MAX_Angular_project/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.css']
+})
+export class ServerAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
