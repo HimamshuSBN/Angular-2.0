fe04b2f8ddba8ad74d1db53554d1a14606e33e86
Himamshu Bengaluru <hbn@Himamshus-MBP.lan>
2023-02-08 20:15:54 -0600

    directives added to the code ngStlye, ngClass,ngFor

--- Angular/MAX_Angular_project/src/app/server/server.component.ts
+++ Angular/MAX_Angular_project/src/app/server/server.component.ts
@@ -3,14 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  // styleUrls: ['./server.component.css'],
+  styles: [`
+  .online{
+    color: white
+  }`
+  ]
 })
 export class ServerComponent {
 
   serverId = 10;
   serverStatus = 'offline'
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus(){
     return this.getServerStatus;
+  }
+  
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
   }
